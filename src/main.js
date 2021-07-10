4521696edb6e904d55228b2101b4089d89a58e68
Lowie Anthonissen <lowie@giveme.media>
2021-07-10 21:16:21 +0200

    updated to use rainfall values

--- src/main.js
+++ src/main.js
@@ -1,9 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-import 'bootstrap/dist/css/bootstrap.css';
-
-App.use(BootstrapVue);
 
 createApp(App).mount('#app')
